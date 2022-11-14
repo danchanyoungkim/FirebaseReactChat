f7e6f718c9dd73a0dc06d5062dd591ca4978839d
Dan Kim <danchanyoungkim@gmail.com>
2022-11-13 22:02:44 -0500

    Fix Chatmessage config

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -59,30 +59,61 @@ function SignOut() {
 }
 
 function ChatRoom() {
-
+  const dummy = useRef();
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
 }
 
 function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
 
-  return <p></p>
-}
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
-return (
-  <>
-    <div>
-      {messages && messages.map(mes => <ChatMessage key={msg.id} message={msg} />)}
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
     </div>
-  </>
-)
-
-function ChatMessage(props) {
-  const { text, uid } = props.message;
-
-  return <p>{text}</p>
+  </>)
 }
 
 export default App;
